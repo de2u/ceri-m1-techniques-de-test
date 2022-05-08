5611a7024434abd55cbf6b0c5d17ce28a572d7e5
de2u <45523885+de2u@users.noreply.github.com>
2022-05-08 16:55:08 +0200

    TP4 YEP

--- src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
@@ -4,7 +4,7 @@ public class PokemonTrainerFactory implements IPokemonTrainerFactory {
     @Override
     public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
         IPokemonMetadataProvider metadataProvider = new PokemonMetadataProvider();
-        IPokemonFactory pokemonFactory = new RocketPokemonFactory();
+        IPokemonFactory pokemonFactory = new PokemonFactory(metadataProvider);
         return new PokemonTrainer(name, team, pokedexFactory.createPokedex(metadataProvider, pokemonFactory));
     }
 }
--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -7,18 +7,20 @@ import org.mockito.Mockito;
 
 public class IPokemonFactoryTest {
 
+    IPokemonMetadataProvider mockedMetadataProvider;
     IPokemonFactory pokemonFactory;
     Pokemon pokemon;
 
     @Before
     public void init() {
-        pokemon = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 4, 100);
-        pokemonFactory = new RocketPokemonFactory();
+        mockedMetadataProvider = new PokemonMetadataProvider();
+        pokemon = new Pokemon(0, "Aquali", 186, 168, 260, 2729, 202, 5000, 4, 100);
+        pokemonFactory = new PokemonFactory(mockedMetadataProvider);
         //Mockito.when(mockedPokemonFactory.createPokemon(133, 2729, 202, 5000, 4)).thenReturn(pokemon);
     }
 
     @Test
-    public void createPokemonTest() {
-        Assert.assertEquals(pokemon.getIndex(), pokemonFactory.createPokemon(133, 2729, 202, 5000, 4).getIndex());
+    public void createPokemonTest() throws PokedexException {
+        Assert.assertEquals(pokemon.getIndex(), pokemonFactory.createPokemon(0, 2729, 202, 5000, 4).getIndex());
     }
 }
